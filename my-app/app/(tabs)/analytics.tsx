373948fa0e9215ddd921b9fbdb122e37c1346000
Emanuel  Chirico <emanuelchirico03@gmail.com>
2025-07-05 10:49:31 +0200

    Applicazione sviluppata con backend

--- my-app/app/(tabs)/analytics.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from 'react-native';
-export default function AnalyticsScreen() {
-  return (
-    <View style={{ flex: 1, padding: 16 }}>
-      <Text style={{ fontSize: 24, fontWeight: 'bold' }}>Statistiche</Text>
-      {/* TODO: implement charts and map */}
-    </View>
-  );
-}
